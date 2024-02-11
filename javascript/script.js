d74b4ed59e9099f782a285b3ddc5a5961938802f
Oskar Swat <oskar.swat@tlen.pl>
2024-02-11 19:39:29 +0100

    Working page

---
+++ javascript/script.js
@@ -0,0 +1,14 @@
+function Zamknij()
+{
+    document.getElementById("footer").style.display = 'none';
+}
+
+function Budowa()
+{
+    alert('Strona w budowie!');
+}
+
+function Close()
+{
+    close();
+}
